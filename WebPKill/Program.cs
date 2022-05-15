3d0c1eb9deb103cb11a2a7d2b21350b52500e5ba
NarryG <3047793+NarryG@users.noreply.github.com>
2022-05-15 14:25:11 -0400

    Fuck WebP

---
+++ WebPKill/Program.cs
@@ -0,0 +1,19 @@
+﻿using SixLabors.ImageSharp;
+if (args.Length == 0)
+    return; 
+
+foreach(var arg in args)
+{
+    try
+    {
+        using (var image = Image.Load(arg))
+        {
+            var outPath = Path.GetFileNameWithoutExtension(arg) + ".png";
+            image.SaveAsPng(outPath);
+        }
+        File.Delete(arg);
+    }catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
